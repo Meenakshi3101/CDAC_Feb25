5e1645790bc9e990614a76f4c6a8ab590fa5813b
Meenakshi  Lokhande <126422374+Meenakshi3101@users.noreply.github.com>
2025-02-22 11:31:21 +0530

    Add files via upload

---
+++ Logic_Building/Assignment_1_Day2/AreaOfSq.java
@@ -0,0 +1,9 @@
+class AreaOfSq{
+public static void main(String[] args){
+int side = 6;
+
+int area = side * side; //calculate area 
+
+System.out.println("Area of square with side " + side + "is : " + area);
+}
+}
---
+++ Logic_Building/Assignment_1_Day2/GoodMorning.java
@@ -0,0 +1,12 @@
+
+class GoodMorning{
+public static void main(String[] args){
+int hour = 7;
+
+if(hour>=5 && hour<12){
+System.out.println("Good Morning");
+}else{
+System.out.println("not an Good Morning");
+}
+}
+}
