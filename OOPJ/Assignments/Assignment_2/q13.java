9a9db9623adf68349f7b3f853f57339bf8a5f92e
Meenakshi  Lokhande <126422374+Meenakshi3101@users.noreply.github.com>
2025-03-11 11:55:28 +0530

    Add files via upload

---
+++ OOPJ/Assignments/Assignment_2/q13.java
@@ -0,0 +1,14 @@
+public class q13{
+	public static void main(String[] args){
+		abs(4);
+		abs(-5);
+	}
+	
+	public static void abs(int num){ 
+		int mask = num >> 5; 
+		int abs = (num + mask) ^ mask;
+		System.out.println(abs);
+	} 
+}	
+
+
---
+++ OOPJ/Assignments/Assignment_2/q5.java
@@ -0,0 +1,11 @@
+public class q5{
+	public static void main(String[] args){
+		int x = 3, y = 9;
+		System.out.println("Before Swapping: x = " + x + ", Y = " + y); 
+		x += y;
+		y -= x;
+		y = -y;
+		x -= y;
+		System.out.println("After Swapping: x = " + x + ", y = " + y); 
+	}
+}
